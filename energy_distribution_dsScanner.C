a7489fcec9feaea7a0b3aa590da6b541bf6a4973
Devi Adhikari <adhidevi@ifarm1901.jlab.org>
2021-11-28 15:39:41 -0500

    simple changes

--- energy_distribution_dsScanner.C
+++ energy_distribution_dsScanner.C
@@ -2,8 +2,8 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-    TString rootfile_dir = "/volatile/halla/parity/adhidevi/remoll_rootfiles/default-geo";
-    const string spTit[] = {"e-/#pi-","e+/#pi+","#gamma","neutron","e-/e+ E>1","primary e E>1"};
+    TString rootfile_dir = "/volatile/halla/moller12gev/devi/remoll_rootfiles/default-geo";
+    const string spTit[] = {"e-/#pi-","e+/#pi+","#gamma","neutron","e-/e+ E>1","primary E>1"};
     const int nSp = sizeof(spTit)/sizeof(*spTit);
     const string spH[nSp] ={"epiM","epiP","g","n","ee1","pri1"};
     map<int,int> spM {{11,1},{-211,1},{-11,2},{211,2},{22,3},{2112,4}};
@@ -12,13 +12,13 @@
     map<int,int> dtM {{28,1},{176,2}};
     const int nDet = sizeof(detH)/sizeof(*detH);
     int Det[nDet] = {28,176};
-    double rmin[nDet] = {920,600};
+    double rmin[nDet] = {920,500};
     double rmax[nDet] = {1060,700};
     TH1D* eRate[nSp][nDet];
     void niceLogBins(TH1*);
     TFile* outfile;
     int beamGen(1);
-    const string tgt_gen_config = "Optics1_beam_magOFF_V5";
+    const string tgt_gen_config = "Optics1_beam_magOFF_V6";
 void energy_distribution_dsScanner(){
    gStyle->SetOptStat(0);
    for(int iSp=0;iSp<nSp;iSp++){
@@ -28,7 +28,7 @@ void energy_distribution_dsScanner(){
       niceLogBins(eRate[iSp][iDet]);
    }
    }
-    outfile = new TFile(Form("./rootfiles/ds_scanner_KE_dist_%s.root",tgt_gen_config.c_str()),"recreate");
+    outfile = new TFile(Form("./rootfiles/ds_scanner_kinE_dist_%s.root",tgt_gen_config.c_str()),"recreate");
     int nfile=0;
     Long64_t nentry=0;
     long nTotEv=0;
@@ -41,8 +41,8 @@ void energy_distribution_dsScanner(){
        }
        TFile *fin = TFile::Open(infile.c_str(),"READ");
        if(fin->TestBit(TFile::kRecovered)){
-       cout<<Form("Skipping %s. Recovered file.",infile.c_str())<<endl;
-       fin->Close(); delete fin; return 0;
+         cout<<Form("Skipping %s. Recovered file.",infile.c_str())<<endl;
+         continue;
        }
        nfile++;
        
@@ -58,6 +58,8 @@ void energy_distribution_dsScanner(){
        remollBeamTarget_t *bm =0;
        remollEvent_t *ev =0;
        Double_t rate =0;
+       Double_t phi = 0;
+       Double_t modphi = 0;
        T->SetBranchAddress("hit", & hit);
        T->SetBranchAddress("part", & part);
        T->SetBranchAddress("bm", & bm);
@@ -74,7 +76,11 @@ void energy_distribution_dsScanner(){
            if(sp==-1) continue;
            int dt = dtM[int(hit->at(j).det)]-1;
            if(dt==-1) continue;
-           
+           phi = hit->at(j).ph;
+           if(phi<0) phi +=2.0*TMath::Pi();
+           modphi = fmod(phi,2.0*TMath::Pi()/7.0);
+           if(modphi<3.0*TMath::Pi()/28.0 || modphi>5.0*TMath::Pi()/28.0) continue;
+
            if(hit->at(j).r>=rmin[dt] && hit->at(j).r<=rmax[dt]){
              eRate[sp][dt]->Fill(hit->at(j).k,rate);
              if(hit->at(j).k>1 && (hit->at(j).pid==11 || hit->at(j).pid==-11)){
@@ -117,10 +123,10 @@ void energy_distribution_dsScanner(){
     latex.SetTextColor(kRed+3);
     latex.DrawLatex(0.12,0.85,"Radial Cut::");
     latex.DrawLatex(0.12,0.80,Form("[%.0f,%.0f]mm",rmin[iDet],rmax[iDet]));
-    c_E[iDet]->SaveAs(Form("./temp/%s_kinetic_energy_det%d.pdf",tgt_gen_config.c_str(),Det[iDet]));
+    c_E[iDet]->SaveAs(Form("./temp/%s_kinE_det%d.pdf",tgt_gen_config.c_str(),Det[iDet]));
     }
-    gSystem->Exec(Form("pdfunite ./temp/%s_kinetic_energy_det*.pdf ./plots/%s_kinE_det%d_det%d.pdf",tgt_gen_config.c_str(),tgt_gen_config.c_str(),Det[0],Det[1]));
-    gSystem->Exec(Form("rm -rf ./temp/%s_kinetic_energy_det*.pdf",tgt_gen_config.c_str()));
+    gSystem->Exec(Form("pdfunite ./temp/%s_kinE_det*.pdf ./plots/%s_kinE_det%d_det%d.pdf",tgt_gen_config.c_str(),tgt_gen_config.c_str(),Det[0],Det[1]));
+    gSystem->Exec(Form("rm -rf ./temp/%s_kinE_det*.pdf",tgt_gen_config.c_str()));
 
 }
 
